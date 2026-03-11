cabed714880b59067b3d519d7091037e03aaacb6
Nauman Riaz Chaudhry <nauman.riaz@gmail.com>
2026-03-11 10:45:34 +0500

    Update

--- src/main/java/com/cwiztech/sectionc/controller/transportRouteController.java
+++ src/main/java/com/cwiztech/sectionc/controller/transportRouteController.java
@@ -1,7 +1,11 @@
 package com.cwiztech.sectionc.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cwiztech.sectionc.model.TransportRoute;
 import com.cwiztech.sectionc.repository.transportRouteRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @CrossOrigin
@@ -40,16 +46,57 @@ public class transportRouteController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	private TransportRoute insert(@RequestBody TransportRoute data) {
-		TransportRoute transportroute = transportrouterepository.saveAndFlush(data);
-		return transportroute;
+	private String insert(@RequestBody String data) throws JsonProcessingException {
+		return insertupdateAll(data);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	private TransportRoute update(@PathVariable Long id, @RequestBody TransportRoute data) {
-		TransportRoute transportroute = transportrouterepository.saveAndFlush(data);
-		return transportroute;
+	private String update(@PathVariable Long id, @RequestBody String data) throws JsonProcessingException {
+		return insertupdateAll(data);
 	}
 
+	@RequestMapping(method = RequestMethod.PUT)
+	private String insertupdate(@RequestBody String data) throws JsonProcessingException, JSONException {
+		JSONArray jsonTransportRoutes = new JSONArray(data);
+		JSONArray jsonAPIResponse = new JSONArray();
+		
+		for (int i = 0; i < jsonTransportRoutes.length(); i++) {
+			jsonAPIResponse.put(insertupdateAll(jsonTransportRoutes.get(i).toString()));
+		}
+
+		return jsonAPIResponse.toString();
+	}
+
+	private String insertupdateAll(String data) throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		String rtnAPIResponse="Invalid Resonse";
+		JSONObject jsonTransportRoute = new JSONObject(data);
+		TransportRoute transportroute = new TransportRoute();
+		
+		if (!jsonTransportRoute.has("transportroute_CODE") || jsonTransportRoute.isNull("transportroute_CODE"))
+			return "transportroute_CODE is missing!";
+			
+		if (!jsonTransportRoute.has("transportroute_DESC") || jsonTransportRoute.isNull("transportroute_DESC"))
+			return "transportroute_DESC is missing!";
+
+		if ((!jsonTransportRoute.has("routetype_ID") || jsonTransportRoute.isNull("routetype_ID")) && 
+				(!jsonTransportRoute.has("routetype_CODE") || jsonTransportRoute.isNull("routetype_CODE")))
+			return "routetype_ID/routetype_CODE is missing!";
+
+		transportroute.setTRANSPORTROUTE_CODE(jsonTransportRoute.getString("transportroute_CODE"));
+		transportroute.setTRANSPORTROUTE_DESC(jsonTransportRoute.getString("transportroute_DESC"));
+		transportroute.setROUTETYPE_ID(jsonTransportRoute.getLong("routetype_ID"));
+		
+		if (jsonTransportRoute.has("colour_ID") && !jsonTransportRoute.isNull("colour_ID"))
+			transportroute.setCOLOUR_ID(jsonTransportRoute.getLong("colour_ID"));
+		
+		if (jsonTransportRoute.has("isactive") && !jsonTransportRoute.isNull("isactive"))
+			transportroute.setISACTIVE(jsonTransportRoute.getString("isactive"));
+		
+		transportroute = transportrouterepository.saveAndFlush(transportroute);
+		rtnAPIResponse = mapper.writeValueAsString(transportroute);
+		
+		return rtnAPIResponse;
+	}
 }
 
